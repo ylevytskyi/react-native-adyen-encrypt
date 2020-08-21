90ed01d8442608493095be7086697bfc9e7b4c91
yurii <yuriy.levytskyy@gmail.com>
2020-08-21 14:11:55 +0300

    added callbacks

--- lib/AdyenEncryptor.ts
+++ lib/AdyenEncryptor.ts
@@ -1,96 +1,101 @@
 import { NativeModules, NativeEventEmitter } from "react-native"
 
 interface CardForm {
-  cardNumber: string
-  securityCode: string
-  expiryMonth: string
-  expiryYear: string
+    cardNumber: string
+    securityCode: string
+    expiryMonth: string
+    expiryYear: string
 }
 
 interface EncryptedCard {
-  encryptedCardNumber: string
-  encryptedExpiryMonth: string
-  encryptedExpiryYear: string
-  encryptedSecurityCode: string
+    encryptedCardNumber: string
+    encryptedExpiryMonth: string
+    encryptedExpiryYear: string
+    encryptedSecurityCode: string
 }
 
 const {
-  AdyenEncryptor: NativeAdyenEncryptor,
-  RNAdyenEventEmitter
+    AdyenEncryptor: NativeAdyenEncryptor,
+    RNAdyenEventEmitter
 } = NativeModules
 
 class AdyenEncryptor {
-  private emitter: NativeEventEmitter
-  constructor(private adyenPublicKey: string) {
-    this.emitter = new NativeEventEmitter(RNAdyenEventEmitter)
-  }
-
-  encryptCard(cardForm: CardForm): Promise<EncryptedCard> {
-    const data = {
-      ...cardForm,
-      publicKey: this.adyenPublicKey
+    private emitter: NativeEventEmitter
+    constructor(private adyenPublicKey: string) {
+        this.emitter = new NativeEventEmitter(RNAdyenEventEmitter)
     }
-    const promise = new Promise<EncryptedCard>((resolve, reject) => {
-      const successSubscription = this.emitter.addListener(
-        "AdyenCardEncryptedSuccess",
-        (result: EncryptedCard) => {
-          successSubscription.remove()
-          resolve(result)
+
+    encryptCard(cardForm: CardForm): Promise<EncryptedCard> {
+        const data = {
+            ...cardForm,
+            publicKey: this.adyenPublicKey
         }
-      )
-      const errorSubscription = this.emitter.addListener(
-          "AdyenCardEncryptedError",
-          (result: EncryptedCard) => {
-            errorSubscription.remove()
-            reject(result)
-          }
-      )
-      NativeAdyenEncryptor.encryptWithData(data)
-    })
-    return promise
-  }
+        const promise = new Promise<EncryptedCard>((resolve, reject) => {
+            const successSubscription = this.emitter.addListener(
+                "AdyenCardEncryptedSuccess",
+                (result: EncryptedCard) => {
+                    successSubscription.remove()
+                    resolve(result)
+                }
+            )
+            const errorSubscription = this.emitter.addListener(
+                "AdyenCardEncryptedError",
+                (result: EncryptedCard) => {
+                    errorSubscription.remove()
+                    reject(result)
+                }
+            )
+            NativeAdyenEncryptor.encryptWithData(data)
+        })
+        return promise
+    }
 
-  identify(token: String, paymentData: String): Promise<any> {
-    const promise = new Promise<any>((resolve, reject) => {
-        const successSubscription = this.emitter.addListener(
-          "AdyenCardEncryptedSuccess",
-          (result: any) => {
-            successSubscription.remove();
-            resolve(result)
-          }
-        )
-        const errorSubscription = this.emitter.addListener(
-            "AdyenCardEncryptedError",
-            (result: any) => {
-              errorSubscription.remove();
-              reject(result);
-            }
-        )
-        NativeAdyenEncryptor.identify(token, paymentData);
-      });
-    return promise;
-}
+    identify(token: String, paymentData: String, callbackSuccess, callbackError): Promise<any> {
+        const promise = new Promise<any>((resolve, reject) => {
+            const successSubscription = this.emitter.addListener(
+                "AdyenCardEncryptedSuccess",
+                (result: any) => {
+                    callbackSuccess(result);
+                    successSubscription.remove();
+                    resolve(result)
+                }
+            )
+            const errorSubscription = this.emitter.addListener(
+                "AdyenCardEncryptedError",
+                (result: any) => {
+                    callbackError(result);
+                    errorSubscription.remove();
+                    reject(result);
+                }
+            )
+            NativeAdyenEncryptor.identify(token, paymentData);
+        });
+        return promise;
+    }
 
-challenge(token: String, paymentData: String): Promise<String> {
-    const promise = new Promise<String>((resolve, reject) => {
-        const successSubscription = this.emitter.addListener(
-          "AdyenCardEncryptedSuccess",
-          (result: String) => {
-            successSubscription.remove()
-            resolve(result)
-          }
-        );
-        const errorSubscription = this.emitter.addListener(
-            "AdyenCardEncryptedError",
-            (result: String) => {
-              errorSubscription.remove()
-              reject(result)
-            }
-        );
-        NativeAdyenEncryptor.challenge(token, paymentData);
-      });
-    return promise;
-}
+    challenge(token: String, paymentData: String, callbackSuccess, callbackError): Promise<String> {
+        console.log('on challenge challenge')
+        const promise = new Promise<String>((resolve, reject) => {
+            const successSubscription = this.emitter.addListener(
+                "AdyenCardEncryptedSuccess",
+                (result: String) => {
+                    callbackSuccess(result);
+                    successSubscription.remove()
+                    resolve(result)
+                }
+            );
+            const errorSubscription = this.emitter.addListener(
+                "AdyenCardEncryptedError",
+                (result: String) => {
+                    callbackError(result);
+                    errorSubscription.remove()
+                    reject(result)
+                }
+            );
+            NativeAdyenEncryptor.challenge(token, paymentData);
+        });
+        return promise;
+    }
 
 }
 
